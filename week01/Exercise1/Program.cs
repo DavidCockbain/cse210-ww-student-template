bc58b1d1095d69189947ffd39add58b669b371bd
David Cockbain <davidjcockbain@gmail.com>
2025-05-08 12:14:01 +0200

    exercise completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,7 +3,13 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+    {   //ask for first name.
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        //ask for last name.
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        //display name in james bond stile.
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
